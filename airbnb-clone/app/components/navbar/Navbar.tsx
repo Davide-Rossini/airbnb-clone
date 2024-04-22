184e35eaff56858b316ce5e708bc7274a7525b7a
DavozGamer <davo.z.gam3r@gmail.com>
2024-04-22 19:20:27 +0200

    navbar basic styles

--- airbnb-clone/app/components/navbar/Navbar.tsx
+++ airbnb-clone/app/components/navbar/Navbar.tsx
@@ -1,9 +1,10 @@
 const Navbar = () => {
     return (  
-        <div>
-            I am a Navbar
+        <div className="fixed w-full bg-white z-10 shadow-sm">
+            <div className="py-4
+            border-b-[1px]"></div>
         </div>
     );
 }
- 
+
 export default Navbar;
