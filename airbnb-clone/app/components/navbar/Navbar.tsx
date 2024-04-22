2a71defd69cc5aaf3a21afe5d9f6b88732d201ed
DavozGamer <davo.z.gam3r@gmail.com>
2024-04-22 19:21:31 +0200

    container

--- airbnb-clone/app/components/navbar/Navbar.tsx
+++ airbnb-clone/app/components/navbar/Navbar.tsx
@@ -2,7 +2,11 @@ const Navbar = () => {
     return (  
         <div className="fixed w-full bg-white z-10 shadow-sm">
             <div className="py-4
-            border-b-[1px]"></div>
+            border-b-[1px]">
+                <Container>
+                    
+                </Container>
+            </div>
         </div>
     );
 }
