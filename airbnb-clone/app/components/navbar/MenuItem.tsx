727556e48eececcc3c6fe3842d282a6873bb11e2
DavozGamer <davo.z.gam3r@gmail.com>
2024-04-22 20:36:38 +0200

    login element added

--- airbnb-clone/app/components/navbar/MenuItem.tsx
+++ airbnb-clone/app/components/navbar/MenuItem.tsx
@@ -10,7 +10,15 @@ const MenuItem: React.FC<MenuItemProps> = ({
     label
 }) => {
     return ( 
-        <div>
+        <div
+        onClick={onClick}
+        className="
+        px-4
+        py-3
+        hover:bg-neutral-100
+        transition
+        font-semibold">
+            {label}
 
         </div>
      );
