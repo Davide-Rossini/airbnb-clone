47dc3ae807463e5d779498f55a27a9f4e065d024
DavozGamer <davo.z.gam3r@gmail.com>
2024-04-23 17:52:18 +0200

    second button and secondary props name changes

--- airbnb-clone/app/components/modals/Modal.tsx
+++ airbnb-clone/app/components/modals/Modal.tsx
@@ -14,7 +14,7 @@ interface ModalProps {
     actionLabel: string;
     disabled?: boolean;
     secondaryAction?: () => void;
-    secondaryLabel?: string;
+    secondaryActionLabel?: string;
 }
 
 
@@ -28,7 +28,7 @@ const Modal: React.FC<ModalProps> = ({
     actionLabel,
     disabled,
     secondaryAction,
-    secondaryLabel,
+    secondaryActionLabel,
 }) => {
     const [showModal, setShowModal] = useState(isOpen);
     useEffect(() => {
@@ -139,6 +139,13 @@ const Modal: React.FC<ModalProps> = ({
                                 items-center
                                 gap-4
                                 w-full">
+                                    {secondaryAction && secondaryActionLabel &&(
+                                        <Button
+                                        outline 
+                                        disabled={disabled}
+                                        label={secondaryActionLabel}
+                                        onClick={handleSecondaryAction}/>
+                                    )}
                                     <Button 
                                     disabled={disabled}
                                     label={actionLabel}
