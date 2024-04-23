cf150a36afc88bc44c6f89d1517616119e278c23
DavozGamer <davo.z.gam3r@gmail.com>
2024-04-23 16:56:02 +0200

    modal props

--- airbnb-clone/app/components/modals/Modal.tsx
+++ airbnb-clone/app/components/modals/Modal.tsx
@@ -1,7 +1,22 @@
+
+interface ModalProps {
+    isOpen?: boolean;
+    onClose: () => void;
+    onSubmit: () => void;
+    title?: string;
+    body?: string;
+    footer?: string;
+    actionLabel: string;
+    disabled?: boolean;
+    secondaryAction?: () => void;
+    secondaryLabel?: string;
+}
+
+
 const Modal = () => {
     return ( 
         <div>
-            
+
         </div>
      );
 }
