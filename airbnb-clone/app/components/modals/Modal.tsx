b06fa887a81205aa9429607702f7016c22e6b808
DavozGamer <davo.z.gam3r@gmail.com>
2024-04-23 17:30:05 +0200

    footer style and button implemented

--- airbnb-clone/app/components/modals/Modal.tsx
+++ airbnb-clone/app/components/modals/Modal.tsx
@@ -2,6 +2,7 @@
 
 import { useCallback, useEffect, useState } from "react";
 import { IoMdClose } from "react-icons/io";
+import Button from "../Button";
 
 interface ModalProps {
     isOpen?: boolean;
@@ -129,6 +130,18 @@ const Modal: React.FC<ModalProps> = ({
                             </div>
                             <div className="relative p-6 flex-auto">
                                 {body}
+                            </div>
+                            <div className="
+                            flex flex-col gap-2 p-6">
+                                <div className="
+                                flex
+                                flex-row
+                                items-center
+                                gap-4
+                                w-full">
+                                    <Button/>
+                                </div>
+
                             </div>
                         </div>
 
