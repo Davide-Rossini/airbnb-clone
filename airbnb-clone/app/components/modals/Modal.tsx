d6b79b4c472d9231a4edebc09f7e26bfe66ddb10
DavozGamer <davo.z.gam3r@gmail.com>
2024-04-23 17:11:45 +0200

    modal styles

--- airbnb-clone/app/components/modals/Modal.tsx
+++ airbnb-clone/app/components/modals/Modal.tsx
@@ -69,7 +69,19 @@ const Modal: React.FC<ModalProps> = ({
             outline-none
             focus:outline-none
             bg-neutral-800/70">
+                <div className="
+                relative
+                w-full
+                md:w-4/6
+                lg:w-3/6
+                xl:w-2/5
+                my-6
+                mx-auto
+                h-full
+                lg:h-auto
+                md:h-auto">
 
+                </div>
             </div>
         </>
      );
