5f6d3244e03527a722b3e51fa9d18ca6371cc114
DavozGamer <davo.z.gam3r@gmail.com>
2024-04-23 17:16:16 +0200

    content style

--- airbnb-clone/app/components/modals/Modal.tsx
+++ airbnb-clone/app/components/modals/Modal.tsx
@@ -80,7 +80,15 @@ const Modal: React.FC<ModalProps> = ({
                 h-full
                 lg:h-auto
                 md:h-auto">
+                    <div className={`
+                        translate
+                        duration-300
+                        h-full
+                        ${showModal ? 'translate-y-0' : 'translate-y-full'}
+                        ${showModal ? 'opacity-100' : 'opacity-0'}
+                    `}>
 
+                    </div>
                 </div>
             </div>
         </>
