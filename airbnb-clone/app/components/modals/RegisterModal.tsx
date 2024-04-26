b12226d4d1413ba922b80ec1242f23fd9592a811
Davide Rossini <117028559+Davide-Rossini@users.noreply.github.com>
2024-04-26 09:34:00 +0000

    modal configuration into RegisterModal

--- airbnb-clone/app/components/modals/RegisterModal.tsx
+++ airbnb-clone/app/components/modals/RegisterModal.tsx
@@ -10,11 +10,12 @@ import {
     useForm
 } from 'react-hook-form';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
+import Modal from './Modal';
 
 
 const RegisterModal = () => {
     const registerModal = useRegisterModal();
-    const [loading, setIsLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
     const{
         register,
@@ -42,12 +43,19 @@ const RegisterModal = () => {
         .finally(() => {
             setIsLoading(false);
         })
-        
+
     }
 
 
     return ( 
-        <div></div>
+        <Modal
+            disabled={isLoading}
+            isOpen={registerModal.isOpen}
+            title='Register'
+            actionLabel='Continue'
+            onClose={registerModal.onClose}
+            onSubmit={handleSubmit(onSubmit)}
+        />
      );
 }
  
