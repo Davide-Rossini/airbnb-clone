c77129b269cb78fd98382d2ff14d384470100f67
Davide Rossini <117028559+Davide-Rossini@users.noreply.github.com>
2024-05-02 11:45:36 +0000

    auth ui react-hot-toast

--- airbnb-clone/app/components/modals/RegisterModal.tsx
+++ airbnb-clone/app/components/modals/RegisterModal.tsx
@@ -11,6 +11,8 @@ import {
 } from 'react-hook-form';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './Modal';
+import Heading from '../Heading';
+import Input from '../inputs/Input';
 
 
 const RegisterModal = () => {
@@ -48,7 +50,36 @@ const RegisterModal = () => {
 
     const bodyContent = (
         <div className='flex flex-col gap-4'>
-            
+            <Heading
+            title="Welcome to Airbnb"
+            subtitle="Create an account!"
+
+            />
+            <Input
+            id="email"
+            label="Email"
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+            />
+            <Input
+            id="name"
+            label="Name"
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+            />
+            <Input
+            id="password"
+            type="password"
+            label="Password"
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+            />
         </div>
     )
 
@@ -61,7 +92,7 @@ const RegisterModal = () => {
             actionLabel='Continue'
             onClose={registerModal.onClose}
             onSubmit={handleSubmit(onSubmit)}
-            
+            body={bodyContent}
         />
     );
 }
